62497e768e9d9efbd37f01f2e6e942957522a754
SanzaTheMan <sandile.mnukwa@gmail.com>
2018-10-19 10:10:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World-Happy Learning. This is a wonderful day! This is my first Jenkins PHP200 page using OpenShift Online. I love this app</p>'; ?>
+ <?php echo '<p>Hello World-Happy Learning. This is a wonderful day! This is my first Jenkins PHP200 page using OpenShift Online. I love it app</p>'; ?>
  </body>
 </html>
